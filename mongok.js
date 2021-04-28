d303dfcb9d08906f658f1c1218f627a059a18e18
kira2040k <46848151+kira2040k@users.noreply.github.com>
2021-04-28 22:01:06 +0300

    Update mongok.js

--- mongok.js
+++ mongok.js
@@ -53,7 +53,7 @@ const mongok_updateOne = (your_db,coll,name,mode) =>
     });
 }
 
-const mongok_updateOne = (your_db,coll,query) =>
+const mongok_deleteOne = (your_db,coll,query) =>
 {
     return new Promise((resolve, reject) => {
        
@@ -71,9 +71,80 @@ const mongok_updateOne = (your_db,coll,query) =>
           });
     });
 }
+
+const mongok_deleteOne = (your_db,coll,query) =>
+{
+    return new Promise((resolve, reject) => {
+       
+        MongoClient.connect(url,{ useNewUrlParser: true }, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db(your_db);
+            
+            dbo.collection(coll).deleteOne(query, function(err, result) {
+              if (err) throw err;
+              if(result){
+                  resolve(result)
+              }
+              db.close()
+            });
+          });
+    });
+}
+
+const mongok_createCollection = (your_db,coll,your_text) =>
+{
+    return new Promise((resolve, reject) => {
+       
+      MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(your_db);
+        dbo.createCollection(coll, function(err, res) {
+          if (err) throw err;
+          resolve(your_text)
+          db.close();
+        });
+      });
+    });
+}
+
+const mongok_dropCollection = (your_db,coll,your_text) =>
+{
+    return new Promise((resolve, reject) => {
+       
+      MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(your_db);
+        dbo.collection(coll).drop(function(err, delOK) {
+          if (err) throw err;
+          if (delOK) resolve(your_text);
+          db.close();
+        });
+      });
+    });
+}
+
+const mongok_find_with_limit = (your_db,coll,query,limit) =>
+{
+    return new Promise((resolve, reject) => {
+       
+      MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(your_db);
+        dbo.collection(coll).find(query).limit(limit).toArray(function(err, result) {
+          if (err) throw err;
+          resolve(result);
+          db.close();
+        });
+      });
+    });
+}
 module.exports = 
 {
   mongok_findOne,
   mongok_updateOne,
-  mongok_insertOne
+  mongok_insertOne,
+  mongok_deleteOne,
+  mongok_createCollection,
+  mongok_dropCollection,
+  mongok_find_with_limit
 }
