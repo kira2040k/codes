a355ef24d89ef657dc13dde36154fc3e3ed9a11a
kira2040k <46848151+kira2040k@users.noreply.github.com>
2021-08-17 09:10:45 +0300

    Update screenshot.ps1

--- screenshot.ps1
+++ screenshot.ps1
@@ -1,14 +1,43 @@
 [Reflection.Assembly]::LoadWithPartialName("System.Drawing")
-function screenshot([Drawing.Rectangle]$bounds, $path) {
-   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
-   $graphics = [Drawing.Graphics]::FromImage($bmp)
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+function screenshot($path) 
+{
+    $width = 0;
+    $height = 0;
+    $workingAreaX = 0;
+    $workingAreaY = 0;
 
-   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+    $screen = [System.Windows.Forms.Screen]::AllScreens;
 
-   $bmp.Save($path)
+    foreach ($item in $screen)
+    {
+        if($workingAreaX -gt $item.WorkingArea.X)
+        {
+            $workingAreaX = $item.WorkingArea.X;
+        }
 
-   $graphics.Dispose()
-   $bmp.Dispose()
+        if($workingAreaY -gt $item.WorkingArea.Y)
+        {
+            $workingAreaY = $item.WorkingArea.Y;
+        }
+
+        $width = $width + $item.Bounds.Width;
+
+        if($item.Bounds.Height -gt $height)
+        {
+            $height = $item.Bounds.Height;
+        }
+    }
+
+    $bounds = [Drawing.Rectangle]::FromLTRB($workingAreaX, $workingAreaY, $width, $height); 
+    $bmp = New-Object Drawing.Bitmap $width, $height;
+    $graphics = [Drawing.Graphics]::FromImage($bmp);
+
+    $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size);
+
+    $bmp.Save($path);
+
+    $graphics.Dispose();
+    $bmp.Dispose();
 }
-
-$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1000, 900)
