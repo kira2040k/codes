df3b2925643910120837b638939751119a1b478e
kira2040k <46848151+kira2040k@users.noreply.github.com>
2021-09-07 17:58:47 +0300

    Update secure.php

--- secure.php
+++ secure.php
@@ -1,8 +1,12 @@
 <?php
 
+// This code will prevent cross site scripting(XSS) on your website 99%
+// Just include it on php files you have
 foreach ($_GET as $par => $value){
     $_GET[$par] = htmlspecialchars($value);
-    
+   } 
+   foreach ($_POST as $par => $value){
+    $_POST[$par] = htmlspecialchars($value);
    } 
 
 ?>
